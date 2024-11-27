53a61e9438abb40d040b6a773f7083ed6e243cd6
Andrew <abrakadabro17@gmail.com>
2024-11-27 17:29:02 +0100

    hw2 - done

--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -1,49 +1,46 @@
-import React from 'react'
-import { AffairType } from '../../HW2'
-import s from './Affair.module.css'
-import s2 from '../Affairs.module.css'
+import React from 'react';
+import { AffairType } from '../../HW2';
+import s from './Affair.module.css';
+import s2 from '../Affairs.module.css';
 
 type AffairPropsType = {
-    // key не нужно типизировать
-    affair: AffairType
-    deleteAffairCallback: any // need to fix any
-}
+    affair: AffairType; // Дело, которое нужно отобразить
+    deleteAffairCallback: (id: number) => void; // Функция для удаления дела
+};
 
 function Affair(props: AffairPropsType) {
+    // Обработчик кнопки удаления
     const deleteCallback = () => {
-        // need to fix
-    }
+        props.deleteAffairCallback(props.affair._id); // Вызываем callback с _id дела
+    };
 
-    const nameClass = s.name + ' ' + s2[props.affair.priority]
-    const buttonClass = s.closeButton + ' ' + s2[props.affair.priority]
-    const affairClass = s.affair + ' ' + s2[props.affair.priority]
+    // CSS классы для стилизации
+    const nameClass = `${s.name} ${s2[props.affair.priority]}`; // Название дела
+    const buttonClass = `${s.closeButton} ${s2[props.affair.priority]}`; // Кнопка удаления
+    const affairClass = `${s.affair} ${s2[props.affair.priority]}`; // Общий контейнер дела
 
     return (
-        <div
-            id={'hw2-affair-' + props.affair._id}
-            className={affairClass}
-        >
+        <div id={'hw2-affair-' + props.affair._id} className={affairClass}>
+            {/* Название дела */}
             <div id={'hw2-name-' + props.affair._id} className={nameClass}>
-                {/*создаёт студент*/}
-
-                {/**/}
+                {props.affair.name}
             </div>
+
+            {/* Приоритет дела (можно скрыть или отображать) */}
             <div id={'hw2-priority-' + props.affair._id} hidden>
                 {props.affair.priority}
             </div>
 
+            {/* Кнопка удаления */}
             <button
                 id={'hw2-button-delete-' + props.affair._id}
                 className={buttonClass}
-                // need to fix
-
+                onClick={deleteCallback} // Обработчик кнопки удаления
             >
-                {/*текст кнопки могут изменить студенты*/}
-                X
-                {/**/}
+                X {/* Текст кнопки */}
             </button>
         </div>
-    )
+    );
 }
 
-export default Affair
+export default Affair;
