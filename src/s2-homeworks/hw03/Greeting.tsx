6eb377797205e29ffdea9b98068468af0d66839c
Andrew <abrakadabro17@gmail.com>
2024-12-02 23:28:46 +0100

    HW3 - done 1

--- src/s2-homeworks/hw03/Greeting.tsx
+++ src/s2-homeworks/hw03/Greeting.tsx
@@ -32,7 +32,8 @@ const Greeting: React.FC<GreetingPropsType> = (
         lastUserName,
     } // деструктуризация пропсов
 ) => {
-    const inputClass = s.errorInput // need to fix with (? s.errorInput : s.input)
+
+    const inputClass = error ? s.errorInput : s.input
 
     return (
         <div id={'hw3-form'} className={s.greetingForm}>
